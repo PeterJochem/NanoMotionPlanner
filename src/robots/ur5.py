fb3d87475e5f74b1ffba4e4e102442adaf4a4e76
PeterJochem <peterpennrichjochem@gmail.com>
2024-03-27 13:22:58 -0500

    Adds code to wrap the UR meshes.

--- src/robots/ur5.py
+++ src/robots/ur5.py
@@ -1,5 +1,7 @@
 import numpy as np
+import os
 from geometry.kinematic_chain import KinematicOpenChain
+from geometry.mesh import Mesh
 from geometry.primitives.screw import Screw
 from geometry.primitives.transformation import Transformation
 
@@ -28,6 +30,50 @@ class URDimensions:
         self.L2 = mm_to_meters(392.)
         self.H1 = mm_to_meters(89.)
         self.H2 = mm_to_meters(95.)
+
+
+class UR5Meshes:
+
+    path_to_file = os.path.dirname(os.path.abspath(__file__))
+    meshes_path = f"{path_to_file}/meshes/ur5/collision/"
+
+    def __init__(self,
+                 base: str = f"{meshes_path}/base.stl",
+                 shoulder: str = f"{meshes_path}/shoulder.stl",
+                 upper_arm: str = f"{meshes_path}/upperarm.stl",
+                 forearm: str = f"{meshes_path}/forearm.stl",
+                 wrist1: str = f"{meshes_path}/wrist1.stl",
+                 wrist2: str = f"{meshes_path}/wrist2.stl",
+                 wrist3: str = f"{meshes_path}/wrist3.stl"):
+        """Constructor.
+
+            Args:
+                base: str
+                shoulder: str
+                upper_arm: str
+                forearm: str
+                wrist1: str
+                wrist2: str
+                wrist3: str
+        """
+        self.base_path = base
+        self.shoulder_path = shoulder
+        self.upper_arm_path = upper_arm
+        self.forearm_path = forearm
+        self.wrist_1_path = wrist1
+        self.wrist_2_path = wrist2
+        self.wrist_3_path = wrist3
+
+        self.base = Mesh.from_file(self.base_path)
+        self.shoulder = Mesh.from_file(self.shoulder_path)
+        self.upper_arm = Mesh.from_file(self.upper_arm_path)
+        self.forearm = Mesh.from_file(self.forearm_path)
+        self.wrist_1 = Mesh.from_file(self.wrist_1_path)
+        self.wrist_2 = Mesh.from_file(self.wrist_2_path)
+        self.wrist_3 = Mesh.from_file(self.wrist_3_path)
+
+        self.ordered_meshes = [self.base, self.shoulder, self.upper_arm, self.forearm, self.wrist_1, self.wrist_2,
+                               self.wrist_3]
 
 
 def define_ur5_home_transformation() -> Transformation:

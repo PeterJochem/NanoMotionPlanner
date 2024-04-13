94ee881fdc0db4a12fa31f2843e02427eb0b13bc
PeterJochem <peterpennrichjochem@gmail.com>
2024-04-13 16:56:54 -0500

    Refines maual test for visulaizing the robot's meshes.

--- test/geometry/collision_detection/test_kinematic_chain_mesh_collision_detector.py
+++ test/geometry/collision_detection/test_kinematic_chain_mesh_collision_detector.py
@@ -4,7 +4,9 @@ import numpy as np
 from geometry.collision_detection.kinematic_chain_mesh_collision_detector import KinematicChainMeshCollisionDetector
 from geometry.kinematic_chain import KinematicOpenChain
 from geometry.mesh import Mesh
-from robots.ur5 import define_ur5_kinematic_chain, UR5Meshes
+from geometry.primitives.transformation import Transformation
+from geometry.utilities import multiply
+from robots.ur5 import define_ur5_kinematic_chain, UR5Meshes, UR5ZeroAngleTransformations
 
 import open3d as o3d
 import open3d as mylib
@@ -52,29 +54,78 @@ def test_kinematic_chain_mesh_collision_detector(kinematic_chain: KinematicOpenC
 
 def test_dummy():
 
-    return
-
     meshes = ur5_meshes
 
     base_mesh = meshes[0]
     link_2_mesh = meshes[1]
+    link_3_mesh = meshes[2]
+    link_4_mesh = meshes[3]
+    link_5_mesh = meshes[4]
+    link_6_mesh = meshes[5]
 
+
+    Ts = UR5ZeroAngleTransformations().transformations
 
     base_vertices = []
+    base_to_mesh_1 = Transformation.identity()  # multiply(Ts[:0])
     for triangle in base_mesh.triangles:
         for point in triangle:
-            base_vertices.append(point)
+            transformed_point = base_to_mesh_1.transform_point(point)
+            base_vertices.append(transformed_point)
 
     link_2_vertices = []
+    base_to_mesh_2 = multiply(Ts[:5])
     for triangle in link_2_mesh.triangles:
         for point in triangle:
-            transformed_point = ...
+            transformed_point = base_to_mesh_2.transform_point(point)
             link_2_vertices.append(transformed_point)
+
+    link_3_vertices = []
+    base_to_mesh_3 = multiply(Ts[:7])
+    for triangle in link_3_mesh.triangles:
+        for point in triangle:
+            transformed_point = base_to_mesh_3.transform_point(point)
+            link_3_vertices.append(transformed_point)
+
+    link_4_vertices = []
+    base_to_mesh_4 = multiply(Ts[:9])
+    for triangle in link_4_mesh.triangles:
+        for point in triangle:
+            transformed_point = base_to_mesh_4.transform_point(point)
+            link_4_vertices.append(transformed_point)
+
+    link_5_vertices = []
+    base_to_mesh_5 = multiply(Ts[:11])
+    for triangle in link_5_mesh.triangles:
+        for point in triangle:
+            transformed_point = base_to_mesh_5.transform_point(point)
+            link_5_vertices.append(transformed_point)
+
+    link_6_vertices = []
+    base_to_mesh_6 = multiply(Ts[:13])
+    for triangle in link_6_mesh.triangles:
+        for point in triangle:
+            transformed_point = base_to_mesh_6.transform_point(point)
+            link_6_vertices.append(transformed_point)
+
+    robot_vertices = np.array(base_vertices + link_2_vertices)  # + link_3_vertices + link_4_vertices)
+    robot_vertices = np.array(base_vertices + link_2_vertices + link_3_vertices + link_4_vertices + link_5_vertices + link_6_vertices)
 
     base_vertices = np.array(base_vertices)
     link_2_vertices = np.array(link_2_vertices)
+    link_3_vertices = np.array(link_3_vertices)
+    link_4_vertices = np.array(link_4_vertices)
+    link_5_vertices = np.array(link_5_vertices)
 
     #breakpoint()
     base_mesh_triangles = o3d.utility.Vector3dVector(base_vertices)
     link_2_mesh_triangles = o3d.utility.Vector3dVector(link_2_vertices)
+    link_3_mesh_triangles = o3d.utility.Vector3dVector(link_3_vertices)
+
+    robot_mesh_triangles = o3d.utility.Vector3dVector(robot_vertices)
+
     visualize_mesh(base_mesh_triangles, None)
+    visualize_mesh(link_2_mesh_triangles, None)
+    visualize_mesh(robot_mesh_triangles, None)
+
+
